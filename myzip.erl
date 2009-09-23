ed50ab0bd62394c6faf136aa03a64bc476298ce8
yan <yan@debian.(none)>
2009-09-23 14:21:49 +0900

    rename function name

--- myzip.erl
+++ myzip.erl
@@ -1,11 +1,12 @@
 -module(myzip).
--compile(export_all).
+%%-compile(export_all).
+-export([zip/1,zip_longest/1]).
 
 %%L = [[L11,L12,L13,...],[L21,L22,L23,...],...[Ln1,Ln2,Ln3,...]]
-myzip(L) ->
-    myzip_tr(L).
+zip(L) ->
+    zip_tr(L).
 
-myzip_tr(L) ->
+zip_tr(L) ->
     IniAcc0 = array:to_list(array:new(lists:min([length(I) || I <-L ]),{default,[]})),
     Results = [lists:reverse(I) || I <- lists:foldl(fun zipfunc/2, IniAcc0, L )],
     %%io:format("~p~n",[Results]),
@@ -23,10 +24,10 @@ zipfunc_tr([H|T], AccIn, Index, Count) ->
     zipfunc_tr(T, element(1,lists:split(Index-1,AccIn)) ++
                [[H|lists:nth(Index,AccIn)]] ++ element(2,lists:split(Index, AccIn)), Index+1, Count-1).
 
-myzip_longest(L,FillValue) ->
-    myzip_longest_tr(L, FillValue).
+zip_longest(L) ->
+    zip_longest_tr(L).
 
-myzip_longest_tr(L, FillValue) ->
+zip_longest_tr(L) ->
     IniAcc0 = array:to_list(array:new(lists:max([length(I) || I <-L ]),{default,[]})),
     Results = [lists:reverse(I) || I <- lists:foldl(fun zipfunc_longest/2, IniAcc0, L )],
     %%io:format("~p~n",[Results]),
@@ -39,7 +40,7 @@ zipfunc_longest_tr(_,AccIn,_Index,0) ->
     AccIn;
 zipfunc_longest_tr([],AccIn,Index,Count) ->
     zipfunc_longest_tr([],element(1,lists:split(Index-1,AccIn)) ++
-                                  [["-"|lists:nth(Index,AccIn)]] ++ element(2,lists:split(Index,AccIn)), Index+1, Count-1);
+                                  [[undefined|lists:nth(Index,AccIn)]] ++ element(2,lists:split(Index,AccIn)), Index+1, Count-1);
 zipfunc_longest_tr([H|T], AccIn, Index, Count) ->
     zipfunc_longest_tr(T, element(1,lists:split(Index-1,AccIn)) ++
                                   [[H|lists:nth(Index,AccIn)]] ++ element(2,lists:split(Index,AccIn)), Index+1, Count-1).
